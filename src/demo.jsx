9a492862590a010f94850ebf552b7122790e613b
smsalim931 <smsalim931@gmail.com>
2025-07-01 00:58:32 +0530

    first commit

---
+++ src/demo.jsx
@@ -0,0 +1,10 @@
+import Dest from "./test"
+
+const Demo = () =>{
+    return( <>
+    <p>I am a web developer</p>
+    <Dest/>
+    </>)
+}
+
+export default Demo
---
+++ src/test.jsx
@@ -0,0 +1,10 @@
+const Dest = () =>{
+    const a = 10
+    return (
+        <>
+            <p>Value of a: {a}</p>
+        </>
+    )   
+}
+
+export default Dest
